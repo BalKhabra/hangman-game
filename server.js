b4ecc42ebfd4eb345f88fece693ced402147137e
Baljit Khabra <baljitkhabra99@gmail.com>
2023-07-27 10:16:42 -0700

    completed with server

---
+++ server.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Handle requests for the homepage (index.html)
+  if (req.url === '/' || req.url === '/index.html') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading index.html');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    });
+  }
+  // Handle requests for other static files (style.css and script.js)
+  else if (req.url === '/stylesheet/style.css') {
+    const filePath = path.join(__dirname, 'stylesheet', 'style.css');
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading style.css');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(content);
+      }
+    });
+  }
+  else if (req.url === '/script.js') {
+    const filePath = path.join(__dirname, 'script.js');
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end('Error loading script.js');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/javascript' });
+        res.end(content);
+      }
+    });
+  }
+  // Handle other requests (404)
+  else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
